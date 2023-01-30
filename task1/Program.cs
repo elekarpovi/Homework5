28e458f7f00f3ad7a033aea774a0645b7b0f819c
Elena Karpovich <elekarpovi@tut.by>
2023-01-30 15:38:01 +0300

    Added task1

---
+++ task1/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+System.Console.Write("Введите количество элементов в массиве: ");
+int len = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int [len];
+Random rnd = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(1000);
+} 
+
+foreach (var item in array)
+{
+    System.Console.Write(item + ", ");
+}
+System.Console.WriteLine();
+
+int count = 0;
+foreach (var item in array)
+{
+    if (item % 2 == 0)
+    {
+        count++;
+    }
+}
+
+System.Console.WriteLine($"Количество четных чисел в массиве: {count}");
